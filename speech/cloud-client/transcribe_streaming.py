f0d1b2e45e1754486a88776234afc6dcc208cb67
Shane Blackett <shane@blackett.co.nz>
2018-02-15 15:01:55 +1300

    Limit the streaming rate so that if the data is from a true file it doesn't generate a error. RuntimeError: Server error: Audio data is being streamed too fast. Please stream audio data approximately at real time.

--- speech/cloud-client/transcribe_streaming.py
+++ speech/cloud-client/transcribe_streaming.py
@@ -23,8 +23,12 @@ Example usage:
 # [START import_libraries]
 import argparse
 import io
+import sys
 # [END import_libraries]
 
+
+sampleRate = 16000
+bytesPerSample = 2
 
 # [START def_transcribe_streaming]
 def transcribe_streaming(stream_file):
@@ -32,40 +36,57 @@ def transcribe_streaming(stream_file):
     from google.cloud import speech
     from google.cloud.speech import enums
     from google.cloud.speech import types
+
+
+    def timingGenerator(audio_stream, chunkSize):
+        from time import sleep
+        from random import random
+        finished = False
+        while not finished:
+            chunk = audio_stream.read(chunkSize)
+            if random() > 1.9:
+                chunk = audio_stream.read(chunkSize)
+
+            if chunk:
+                sleep(chunkSize/(sampleRate*bytesPerSample))
+                yield types.StreamingRecognizeRequest(audio_content=chunk)
+            else:
+                finished = True
+
     client = speech.SpeechClient()
 
     # [START migration_streaming_request]
     with io.open(stream_file, 'rb') as audio_file:
-        content = audio_file.read()
 
-    # In practice, stream should be a generator yielding chunks of audio data.
-    stream = [content]
-    requests = (types.StreamingRecognizeRequest(audio_content=chunk)
-                for chunk in stream)
+        chunkSize = 32000
+        requests = timingGenerator(audio_file, chunkSize)
 
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code='en-US')
-    streaming_config = types.StreamingRecognitionConfig(config=config)
+        config = types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=sampleRate,
+            language_code='en-US')
+        streaming_config = types.StreamingRecognitionConfig(config=config)
 
-    # streaming_recognize returns a generator.
-    # [START migration_streaming_response]
-    responses = client.streaming_recognize(streaming_config, requests)
-    # [END migration_streaming_request]
-
-    for response in responses:
+        # streaming_recognize returns a generator.
+        # [START migration_streaming_response]
+        responses = client.streaming_recognize(streaming_config, requests)
+        # [END migration_streaming_request]
+    
+        for response in responses:
         # Once the transcription has settled, the first result will contain the
         # is_final result. The other results will be for subsequent portions of
         # the audio.
-        for result in response.results:
-            print('Finished: {}'.format(result.is_final))
-            print('Stability: {}'.format(result.stability))
-            alternatives = result.alternatives
-            # The alternatives are ordered from most likely to least.
-            for alternative in alternatives:
-                print('Confidence: {}'.format(alternative.confidence))
-                print('Transcript: {}'.format(alternative.transcript))
+            print ("Response")
+            for result in response.results:
+                print('Finished: {}'.format(result.is_final))
+                print('Stability: {}'.format(result.stability))
+                alternatives = result.alternatives
+                # The alternatives are ordered from most likely to least.
+                for alternative in alternatives:
+                    print('Confidence: {}'.format(alternative.confidence))
+                    print('Transcript: {}'.format(alternative.transcript))
+ 
+
     # [END migration_streaming_response]
 # [END def_transcribe_streaming]
 
